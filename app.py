5fbcc05350277ae2aa587500b658baea9223f58e
Aidan Gawronski <aidangawronski@gmail.com>
2017-11-12 15:24:24 -0500

    adding matplotlib plot, pandas.

--- app.py
+++ app.py
@@ -1,16 +1,45 @@
+#-------------------------------------------------------------------------------
+from flask import render_template
+import matplotlib
+matplotlib.use("agg")
+import matplotlib.pyplot as plt
 from flask import Flask
-from datetime import datetime
+import pandas as pd
+# import pyflux as pf
+# import quandl
+import base64
+import os
+import io
+
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+def build_plot():
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
+    img = io.BytesIO()
+    # data = quandl.get('LBMA/SILVER', authtoken = 'oTBhHqG_L_3PHuxpLEBW')
+    # data = data.fillna(method = 'ffill')
+    # model = pf.ARIMA(data = data, ar = 5, integ = 1, ma = 4, target = 'USD', family = pf.Normal())
+    # x = model.fit("MLE")
+    # model_summary = base64.b64encode( x.summary() ).decode()
 
-    """.format(time=the_time)
+    # plot1 = model.plot_z(figsize = (15,5))
+    y = [1,2,3,4,5]
+    x = [0,2,1,3,4]
+    plt.plot(x,y)
+
+    plt.savefig(img, format='png')
+    # plot1.savefig(img, format='png')
+    img.seek(0)
+
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    return render_template('main_template.html',
+                            plot_url = plot_url)
+
+
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    app.debug = True
+    app.run()
